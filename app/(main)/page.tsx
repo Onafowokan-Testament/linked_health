c78da87944e6283000aeb4b6bb352aad3b618830
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-10-23 19:41:33 +0100

    implement trending topic sbar

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,6 @@
 import Posts from "@/components/Posts";
 import PostEditor from "@/components/posts/editors/PostEditor";
+import TrendsSidebar from "@/components/TrendsSidebar";
 import prisma from "@/lib/prisma";
 import { postDataInclude } from "@/lib/type";
 import Image from "next/image";
@@ -10,12 +11,15 @@ export default async function Home() {
     orderBy: { createdAt: "desc" },
   });
   return (
-    <main className="w-full min-w-0">
+    <main className="w-full min-w-0 flex gap-5">
       <div className="w-full min-w-0 space-y-5">
         <PostEditor />
         {posts.map((post) => (
           <Posts key={post.id} post={post} />
         ))}
+      </div>
+      <div>
+        <TrendsSidebar />
       </div>
     </main>
   );
