5920163fb15f9d2483c8825e2a49c81f2510a62c
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-10-23 17:57:50 +0100

    create post structure

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,11 +1,21 @@
+import Posts from "@/components/Posts";
 import PostEditor from "@/components/posts/editors/PostEditor";
+import prisma from "@/lib/prisma";
+import { postDataInclude } from "@/lib/type";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await prisma.post.findMany({
+    include: postDataInclude,
+    orderBy: { createdAt: "desc" },
+  });
   return (
-    <main className="w-full bg-red-50">
-      <div className="w-full">
+    <main className="w-full min-w-0">
+      <div className="w-full min-w-0 space-y-5">
         <PostEditor />
+        {posts.map((post) => (
+          <Posts key={post.id} post={post} />
+        ))}
       </div>
     </main>
   );
